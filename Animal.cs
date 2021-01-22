217111e6b18ab19b60f01d2fdcb0ee0bc80c9669
nta228 <64982003+nta228@users.noreply.github.com>
2021-01-22 09:49:08 +0700

    ex2

---
+++ Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Animal
+    {
+        public String Name;
+        public int Weight;
+
+        public Animal(String Name, int Weight)
+        {
+            this.Name = Name;
+            this.Weight = Weight;
+        }
+
+    }
+}
---
+++ AnimalDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class AnimalDemo
+    {
+        static void Main(string[] args)
+        {
+            Animal Tiger = new Animal("Tiger", 100);
+
+            Console.WriteLine("Name:" + Tiger.Name + "," + " Weight " + Tiger.Weight);
+            
+
+            Animal Lion = new Animal("Lion", 200);
+
+            Console.WriteLine("Name:" + Lion.Name + "," + " Weight " + Lion.Weight);
+            
+            Console.ReadLine();
+        }
+    }
+}
